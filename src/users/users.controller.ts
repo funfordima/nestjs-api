c504e98e979e9e3a7c04172a14abf42d8b671ab6
Dmytro Litvinov <d.g.litvinov@gmail.com>
2023-10-20 16:51:42 +0300

    feature: add user model, add create/get user requests

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+import { User } from './users.model';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() dto: CreateUserDto): Promise<User> {
+    return this.usersService.createUser(dto);
+  }
+
+  @Get()
+  getAll(): Promise<User[]> {
+    return this.usersService.getAllUsers();
+  }
+}
